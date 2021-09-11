cfb84a86b4a4a27148849eaed2b396655545c44c
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-09-11 15:34:32 +0200

    onattach

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,8 @@
 .onAttach <- function(...) {
-  msg <- paste("  Dusa, Adrian (2019) QCA with R. A Comprehensive Resource.",
-               "  Springer International Publishing.", sep="\n")
-  msg <- paste(msg, "\n\nTo run the graphical user interface, use: runGUI()\n", sep="")
-  packageStartupMessage("\nTo cite package QCA in publications, please use:\n", msg, "\n")
+  library(ggplot2)
+  extrafont::loadfonts(device="win")
+  theme_set(
+    ggmacronom::theme_macronom()
+  )
+  packageStartupMessage("Theme updated")
 }
