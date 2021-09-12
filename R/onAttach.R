bd0d645ee3a3827e42f0d5296f8637b5ef69540e
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-09-12 08:50:27 +0200

    fix ggtherm

--- R/onAttach.R
+++ R/onAttach.R
@@ -5,11 +5,12 @@
   update_geom_defaults("point", list(size = 1.4, shape = 21, color = "black", fill = ggmacronom::macronom_colors(2)))
   update_geom_defaults("line", list(size = 1.4, color = ggmacronom::macronom_colors(2)))
 
-  ggthemr::define_palette(
-    swatch = macronom_colors(1:10),
-    gradient = c(lower = "#D22211", "#469240")
-  ) %>%
-    ggthemr::ggthemr()
+  ggthemr::ggthemr(
+    ggthemr::define_palette(
+      swatch = macronom_colors(1:10),
+      gradient = c(lower = "#D22211", "#469240")
+    )
+  )
 
   theme_set(
     ggmacronom::theme_macronom()
